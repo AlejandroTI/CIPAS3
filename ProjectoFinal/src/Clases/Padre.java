7daa0caee36e32909ac86abde8d57b411b9311af
Angela Rocha <agrochat@ut.edu.co>
2025-04-12 12:39:15 -0500

    Se creó la clase base FAMILIA con métodos y atributos y dos subclases PADRE e HIJO que heredaron los atributos y métodos de la clase base, también se trabajó en la main usando la subclase HIJO a la que se le agregó el atributo Grado.

---
+++ ProjectoFinal/src/Clases/Padre.java
@@ -0,0 +1,12 @@
+package Clases;
+
+import Clases.Familia;
+
+public class Padre extends Familia {
+    public Padre(String nombre, int edad, String tipodesangre, String rolenlafamilia, String profesion) {
+        super(nombre, edad, tipodesangre, rolenlafamilia, profesion);
+    }
+
+    Padre(){}
+
+}
--- ProjectoFinal/src/Main.java
+++ ProjectoFinal/src/Main.java
@@ -1,15 +1,14 @@
+import Clases.Familia;
+import Clases.Hijo;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Hijo h1= new Hijo("Jaime", 6, "A+", "Hijo", "Estudiante");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        h1.setGrado("Quinto (5)");
+        h1.saludar();
+        h1.informar();
     }
 }
